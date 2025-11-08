7eca52c238ee36749742066ba0b18b339b786473
Christian Peters <christian.peters@gmail.com>
2025-11-08 06:36:49 -0600

    Starter function app

---
+++ SqlManagedIdentity/FunctionApp/HealthCheck.cs
@@ -0,0 +1,25 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunctionApp;
+
+public class HealthCheck
+{
+    private readonly ILogger<HealthCheck> _logger;
+
+    public HealthCheck(ILogger<HealthCheck> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HealthCheck")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+        
+    }
+
+}
