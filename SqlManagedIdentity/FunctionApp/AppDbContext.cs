190ed748fbf325bfe92af20175a2aae87b33b328
Christian Peters <christian.peters@gmail.com>
2025-11-15 17:40:47 -0600

    migration file

--- SqlManagedIdentity/FunctionApp/AppDbContext.cs
+++ SqlManagedIdentity/FunctionApp/AppDbContext.cs
@@ -7,7 +7,9 @@ namespace FunctionApp
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
         public DbSet<TodoItem> TodoItems { get; set; } = null!;
 
@@ -18,6 +20,10 @@ namespace FunctionApp
                 eb.HasKey(e => e.Id);
                 eb.Property(e => e.Title).IsRequired().HasMaxLength(200);
                 eb.Property(e => e.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+                eb.HasData(
+                    new TodoItem { Id = 1, Title = "Sample Task 1", IsComplete = false },
+                    new TodoItem { Id = 2, Title = "Sample Task 2", IsComplete = true }
+                );
             });
         }
     }
---
+++ SqlManagedIdentity/Migration1.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [TodoItems] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(200) NOT NULL,
+    [IsComplete] bit NOT NULL,
+    [CreatedAt] datetime2 NOT NULL DEFAULT (GETUTCDATE()),
+    CONSTRAINT [PK_TodoItems] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251109195246_InitialCreate', N'9.0.10');
+
+COMMIT;
+GO
+
