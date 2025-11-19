e32cb2bdb6f20c468b7548a03f61c38bcb3d66ca
Christian Peters <christian.peters@gmail.com>
2025-11-19 05:30:26 -0600

    Updated health check

--- SqlManagedIdentity/FunctionApp/HealthCheck.cs
+++ SqlManagedIdentity/FunctionApp/HealthCheck.cs
@@ -2,7 +2,6 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace FunctionApp;
@@ -24,22 +23,18 @@ public class HealthCheck
         _logger.LogInformation("C# HTTP trigger function processed a request.");
 
         bool canConnect = false;
-        var errMessage = string.Empty;
         try
         {
-            canConnect = await _db.TodoItems.ToArrayAsync() != null;
+            canConnect = await _db.Database.CanConnectAsync();
         }
         catch (System.Exception ex)
         {
             _logger.LogError(ex, "Database connection check failed");
             canConnect = false;
-            errMessage = ex.Message;
         }
 
         var message = $"Connected: {canConnect}|{Environment.NewLine}" +
-                      $"With connection string: {_db.Database.GetConnectionString()}|{Environment.NewLine}" +
-                      $"Environment: {Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT")}|{Environment.NewLine}" +
-                      $"Error Message: {errMessage}|{Environment.NewLine}";
+                      $"With connection string: {_db.Database.GetConnectionString()}|{Environment.NewLine}";
         return new OkObjectResult(message);
     }
 
